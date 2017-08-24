e0f664f20f2d7718bd737b0da0f234a1e33179ed
Israel Vergara <israel.vergara102824@gmail.com>
2017-08-24 09:34:08 -0600

    se agrega el constructor

---
+++ Libro.java
@@ -0,0 +1,12 @@
+
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    
+    public Libro(String tituloLibro, String autorLibro)
+    {
+        titulo = tituloLibro;
+        autor = autorLibro;
+    }
+}
