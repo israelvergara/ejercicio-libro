c443b9d7cca317428585a26897638207e90e335c
Israel Vergara <israel.vergara102824@gmail.com>
2017-09-01 01:35:20 -0500

    se agrega documentacion

--- Libro.java
+++ Libro.java
@@ -1,4 +1,7 @@
-
+/**
+ * clase Libro contiene la bibliografia de un libro tambien pudes ver los prestamos 
+ * @author Israel Vergara
+ */
 public class Libro
 {
     private String titulo;
@@ -8,6 +11,12 @@ public class Libro
     private String detalles;
     private int prestamos;
     
+    /**
+     * Constructor que inicializa las variables titulo, autor, paginas, 
+     * detalles, numeroDeReferencia  y prestamos.
+     * @param tituloLibro, autorLibro, paginasLibro las cuales contiene titulo 
+     * del libro, autor del libro y numero de paginas respectivamente.
+     */
     public Libro(String tituloLibro, String autorLibro, int paginasLibro)
     {
         titulo = tituloLibro;
@@ -18,22 +27,37 @@ public class Libro
         prestamos = 0;
     }
     
-    //metodo de acceso
+    /**
+     * metodo que muestra el nombre del autor
+     * @return autor variable que contiene el numero del autor
+     */
     public String dimeAutor()
     {
         return autor;
     }
     
+    /**
+     * metodo que muestra el titulo del libro
+     * @return titulo variable que contiene el titulo del libro
+     */
     public String dimeTitulo()
     {
         return titulo;
     }
     
+    /**
+     * metodo que muestra el numero de paginas del libro
+     * @return paginas variable que contiene el numero de paginas del libro.
+     */
     public int dimePaginas()
     {
         return paginas;
     }
     
+    /**
+     * metodo que muestra los detalles del libro.
+     * @return detalles variable que contiene los detalles del libro
+     */
     public String dimeDetalles()
     {
          if (numeroDeReferencia.length() > 0)
@@ -48,6 +72,10 @@ public class Libro
          return detalles;
     }
     
+    /**
+     * metodo que cambia el nuero de referencia.
+     * @param nuevaRef contiene el valor que introduce el usuario
+     */
     public void cambiaNumRef(String nuevaRef)
     {
         if(nuevaRef.length() > 3)
@@ -56,16 +84,27 @@ public class Libro
         }
     }
     
+    /**
+     * metodo que muestra el numero de referencia.
+     * @return numeroDeReferencia variable que contiene elvalor de la referencia.
+     */
     public String dimeNumRef()
     {
         return numeroDeReferencia;
     }
     
+    /**
+     * metodo que incrementa lso prestamos del libro
+     */
     public void prestar()
     {
         prestamos++;
     }
     
+    /**
+     * metodo que muestra los prestamos del libro.
+     * @return prestamos variable que tiene cuantas veces se ha prestado el libro.
+     */
     public int dimePrestamos()
     {
         return prestamos;
